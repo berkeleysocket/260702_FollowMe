faef9676a0f4ce6718b073445155222aa987950d
hwani165 <seungyung.kang@gmail.com>
2026-08-20 08:11:41 +0900

    update : Packages

--- Assets/KSY/Scripts/SeungyungLib/Template/Modules/AgentMovementModule.cs
+++ Assets/KSY/Scripts/SeungyungLib/Template/Modules/AgentMovementModule.cs
@@ -1,6 +1,7 @@
 using SeungyungLib.ModuleSystem.Interface;
 
 using System;
+using SeungyungLib.Core.CustomDebug;
 using SeungyungLib.Core.EventChannelSystem;
 using SeungyungLib.Core.InputSystem;
 using SeungyungLib.Template.EventChannels;
@@ -12,13 +13,28 @@ namespace SeungyungLib.Template.Modules
     {
         [SerializeField] private Rigidbody2D rb;
         [SerializeField] private EventChannelSO playerEventChannel;
-        [SerializeReference] private float speed;
+        
+        
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private float acceleration;
+        [SerializeField] private float deceleration;
+
+        private Vector2 _velocity;
+        private float _axis;
+        private float _currentSpeed;
 
         public event Action<float> OnChangeAxis;
 
         public void Initialize(IModuleOwner owner)
         {
             playerEventChannel.AddListener<MoveInputEvent>(HandleMoveInput);
+        }
+
+        private void FixedUpdate()
+        {
+            CalculateVelocity();
+
+            rb.linearVelocity = _velocity;
         }
 
         private void HandleMoveInput(MoveInputEvent moveInputEvent)
@@ -29,11 +45,28 @@ namespace SeungyungLib.Template.Modules
         public void SetMovementVelocity(float axis) 
         {
             OnChangeAxis?.Invoke(axis);
-            rb.linearVelocity = new Vector2(axis * speed, rb.linearVelocity.y);
+            
+            DebugLogger.Log($"axis: {axis}", Color.yellow);
+            _axis = axis;
         }
 
         public void Jump()
         {
+        }
+
+        private void CalculateVelocity()
+        {
+            if (_axis != 0)
+            {
+                bool isReversing = _currentSpeed < _axis;
+                _currentSpeed = Mathf.Clamp(_currentSpeed - deceleration * Time.fixedDeltaTime, 0f, maxSpeed);
+            }
+            else if (_currentSpeed < maxSpeed)
+            {
+                _currentSpeed = Mathf.Clamp(_currentSpeed + acceleration * Time.fixedDeltaTime, 0f, maxSpeed);
+            }
+            
+            _velocity = new Vector2(_currentSpeed * _axis, rb.linearVelocity.y);
         }
     }
 }
